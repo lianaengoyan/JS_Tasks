e751553cee55e5c14c7fc603d8b9a49415e97959
ԼԻԱՆԱ ԵՆԳՈՅԱՆ <lianaengoyan04@gmail.com>
2025-09-16 18:19:21 +0400

    Update bubble_Sort.js

--- Sorting/bubble_Sort.js
+++ Sorting/bubble_Sort.js
@@ -3,6 +3,7 @@ function bubbleSort(arr) {
     let n = arr.length;
     for(let i = 0; i < n - 1; ++i) {
         let swapped = false; 
+        
         for(let j = 0; j < n - 1 - i; ++j)  {
             if(arr[j] > arr[j + 1]) {
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
