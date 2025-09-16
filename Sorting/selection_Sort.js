b87e69118b5f9d084075dcc040182691c28c33b9
ԼԻԱՆԱ ԵՆԳՈՅԱՆ <lianaengoyan04@gmail.com>
2025-09-16 18:17:33 +0400

    Update selection_Sort.js

--- Sorting/selection_Sort.js
+++ Sorting/selection_Sort.js
@@ -4,14 +4,22 @@ function selectionSort (arr) {
 
     for(let i = 0; i < n; ++i) {
         let minIndex = i;
+        let swapped = false;
+        
         for(let j = i + 1; j < n; ++j) {
             if(arr[minIndex] > arr[j]) {
                 minIndex = j; //stex menq gtnum enq arr[minIndex]-ic poqr tiv ev swapp enq anum
             }
         }
 
-        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]] //swapp-i irakanacumy
-    }
+        if(minIndex !== i) {
+            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]] //swapp-i irakanacumy
+            swapped = true; //swapped ayn npatakov e grac, vor ete array shut sortavorvi, avel chfra
+        }
+        
+        if(!swapped) {
+            break;
+        }
     return arr;
 }
 
