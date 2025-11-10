80ed6929083c5262a843a3a956aee2d6418805fc
Liana Engoyan <lianaengoyan04@gmail.com>
2025-11-10 21:09:39 +0400

    trees

--- Data_Structures/Trees/red_black.js
+++ Data_Structures/Trees/red_black.js
@@ -109,7 +109,24 @@ class RBTree {
         this.#root.color = BLACK;
     }
     
-  
+    #transplant(oldNode, newNode) {
+        if(oldNode.parent === this.#nil) this.#root = newNode;
+        else if(oldNode === oldNode.parent.left) {
+            oldNode.parent.left = newNode;
+        } else {
+            oldNode.parent.right = newNode;
+        }
+
+        newNode.parent = oldNode.parent;
+    }
+
+    #minNode(curr) {
+        while(curr.left !== this.#nil) {
+            curr = curr.left;
+        }
+        return curr;
+    }
+    
     insert(val) {
         const newNode = new TreeNode(val);
         let parentNode = this.#nil;
@@ -140,6 +157,32 @@ class RBTree {
         this.#insertFixUp(newNode);
     }
 
+    search(val) {
+        if(!this.#root) return null;
+        let curr = this.#root;
+
+        while(curr !== this.#nil) {
+            if(val === curr.data) return curr;
+            else if(val < curr.data) {
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+        }
+        return null;
+    }
+
+    minNode() {
+        if(!this.#root) return null;
+        let curr = this.#root;
+
+        while(curr.left !== this.#nil) {
+            curr = curr.left;
+        }
+
+        return curr.data;
+    }
+    
     print(node = this.#root, indent = "", isLeft = true) {
         if (node === this.#nil || node.data === null) return;
     
